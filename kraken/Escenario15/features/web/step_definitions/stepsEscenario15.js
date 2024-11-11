c180abaf007280e09fe5272461681c7939329a63
EdgarMISO <ereemrev1632@gmail.com>
2024-11-10 20:26:04 -0500

    Add files via upload

---
+++ kraken/Escenario15/features/web/step_definitions/stepsEscenario15.js
@@ -0,0 +1,35 @@
+const { Given, When, Then, And }  = require("@cucumber/cucumber");
+const { login }              = require("./pageObjects/login_page.js");
+const { clickOnMembersTab }  = require("./pageObjects/members_page.js");
+const { clickOnFilterTool }  = require("./pageObjects/members_page.js");
+const { writeOnFilterTool }  = require("./pageObjects/members_page.js");
+const { applyFilterTool   }  = require("./pageObjects/members_page.js");
+const { screenCapture }      = require("./utils/screenCapture.js");
+
+Given("I login with {kraken-string} and {kraken-string} - Escenario 15", async function (email, password) {
+    await login(this.driver, email, password);
+    await screenCapture(this.driver, "Escenario15", "login");
+});
+
+When("I click on the Members tab - Escenario 15", async function () {
+    await clickOnMembersTab(this.driver);
+    await screenCapture(this.driver, "Escenario15", "clickOnMembersTab");
+});
+
+When("I click on the Filter Tool", async function () {
+    await clickOnFilterTool(this.driver);
+    await screenCapture(this.driver, "Escenario15", "clickOnFilterTool");
+});
+
+When("I write the Filter", async function () {
+    await writeOnFilterTool(this.driver);
+    await screenCapture(this.driver, "Escenario15", "writeOnFilterTool");
+});
+
+When("I apply the Filter", async function () {
+    await applyFilterTool(this.driver);
+    await screenCapture(this.driver, "Escenario15", "applyFilterTool");
+});
+
+
+
